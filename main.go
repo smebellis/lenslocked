5c71b27bb13b8cddf9548f7907f669a022e77139
Ryan Ellis <ryan.ellis33@gmail.com>
2023-03-30 16:37:24 -0600

    added handler func and simple web server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+// This is the function that is called anytime someone comes to the web server
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to this website</h1>")
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
