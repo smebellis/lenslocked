484c851aff2b630e16e44a9b75daae47d4a23a8a
Ryan Ellis <ryan.ellis33@gmail.com>
2023-04-01 15:13:02 -0600

    Contact page template

--- main.go
+++ main.go
@@ -11,17 +11,14 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-// This is the function that is called anytime someone comes to the web server
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+// Helper function to handle repeated code in handlers
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	// Creates a path that is OS agnostic
-	tplPath := filepath.Join("templates", "home.gohtml")
-
 	//Parse the template
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
-		log.Printf("parsing template: %v", err)
+		log.Printf("processing template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
 		return
 	}
@@ -31,13 +28,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
 	}
+}
+
+// This is the function that is called anytime someone comes to the web server
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Creates a path that is OS agnostic
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
 
 }
 
 // Creating a contact handler function to serve a contact page
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:ryan@test.com\">ryan@test.com</a>.</p>")
+
+	// Creates a path that is OS agnostic
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 // Exercise to implement an FAQ page
