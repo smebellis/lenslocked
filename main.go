ae9fb020de5d4aade760b0425b7cb43c6807e5d6
Ryan Ellis <ryan.ellis33@gmail.com>
2023-04-01 08:16:45 -0600

    completed section two

--- main.go
+++ main.go
@@ -5,15 +5,61 @@ import (
 	"net/http"
 )
 
+// Implement our own custom router
+type Router struct{}
+
+// Method on custom router struct
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
 // This is the function that is called anytime someone comes to the web server
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to this awesome website</h1>")
 }
 
-func main() {
-	http.HandleFunc("/", handlerFunc)
+// Creating a contact handler function to serve a contact page
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:ryan@test.com\">ryan@test.com</a>.</p>")
+}
 
+// Exercise to implement an FAQ page
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+		<li>
+			<b>Is there a free version?</b>
+			Yes! We offer a free trial for 30 days on any paid plans.
+		</li>
+		<li>
+			<b>What are your support hours?</b>
+			We have support staff answering emails 24/7, though response
+			times may be a bit slower on weekends.
+		</li>
+		<li>
+			<b>How do I contact support?</b>
+			Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+		</li>
+	</ul>
+	`)
+}
+
+func main() {
+
+	var router Router
 	fmt.Println("Starting server on port 3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 
 }
