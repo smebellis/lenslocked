18e418d8153af72d6e85abb34f75353ec5a64caa
Ryan Ellis <ryan.ellis33@gmail.com>
2023-04-03 19:43:04 -0600

    deleted static method

--- controllers/static.go
+++ controllers/static.go
@@ -6,14 +6,6 @@ import (
 	"github.com/smebellis/lenslocked/views"
 )
 
-type Static struct {
-	Template views.Template
-}
-
-func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	static.Template.Execute(w, nil)
-}
-
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
