53d17364cbcaa00a6a9452b838120637ec8d8bb2
Ryan Ellis <ryan.ellis33@gmail.com>
2023-04-03 18:36:30 -0600

    added the views folder

--- main.go
+++ main.go
@@ -2,32 +2,28 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/smebellis/lenslocked/views"
 )
 
 // Helper function to handle repeated code in handlers
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	//Parse the template
-	tpl, err := template.ParseFiles(filepath)
+	tpl, err := views.Parse(filepath)
 	if err != nil {
 		log.Printf("processing template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
 		return
 	}
 
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
-	}
+	tpl.Execute(w, nil)
+
 }
 
 // This is the function that is called anytime someone comes to the web server
