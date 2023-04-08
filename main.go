645bda047d42c1916e59fc4e83b30ea0bab27839
Ryan Ellis <ryan.ellis33@gmail.com>
2023-04-07 19:40:03 -0600

    tailwind implementation

--- main.go
+++ main.go
@@ -15,11 +15,13 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "home.gohtml", "layout-parts.gohtml"))))
+		views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 
-	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(
+		views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
